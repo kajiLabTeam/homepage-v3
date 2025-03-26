c0b9d28b627140dc60a71b0d91b48833d4d1a84d
SatooRu65536 <ryuto65536@gmail.com>
2025-03-27 02:12:28 +0900

    wip: メタデータの設定

---
+++ src/consts/meta.ts
@@ -0,0 +1,16 @@
+import ogp from '@/assets/ogp.png';
+import favicon from '@/assets/favicon.png';
+
+export const FAVICON: ImageMetadata = favicon;
+export const FAVICON_TYPE = `image/${FAVICON.format}`;
+export const OGP: ImageMetadata = ogp;
+export const OGP_TYPE = `image/${OGP.format}`;
+export const OGP_WIDTH = OGP.width.toString();
+export const OGP_HEIGHT = OGP.height.toString();
+export const SITE_NAME = '愛知工業大学 梶研究室';
+export const KEY_WORDS = ['愛知工業大学', '愛工大', '梶研究室', '行動情報科学研究室'];
+export const CATEGORY = 'website';
+export const DESCRIPTION =
+  '愛知工業大学 梶研究室の公式Webサイトです。研究実績や日常生活についての記事を掲載しています。';
+
+export const TWITTER_CARD_TYPE = 'summary_large_image';

b09c5a6b6e7da789997f177a303767019f666bdd
SatooRu65536 <ryuto65536@gmail.com>
2025-04-18 19:11:38 +0900

    feat: 418を返す

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import remarkImgToMarkdown from './src/plugins/img-to-markdown';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kajilab.net/',
   integrations: [sitemap()],
+
   markdown: {
     remarkPlugins: [remarkImgToMarkdown],
   },
+
   image: {
     domains: ['img.esa.io'],
   },
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -22,7 +27,20 @@ export default defineConfig({
       },
     },
   },
+
   devToolbar: {
     enabled: false,
   },
+
+  adapter: cloudflare({
+    routes: {
+      extend: {
+        include: [
+          {
+            pattern: '/teapot',
+          },
+        ],
+      },
+    },
+  }),
 });
